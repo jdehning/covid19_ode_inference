8d3e36170f59fdd0557f2789e4eda10d28e8392f
Jonas <jonas.dehning@ds.mpg.de>
2023-07-31 00:26:47 +0200

    jax functions with pytrees as parameters can now be converted to a PyMC Op

--- covid19_ode_inference/compartmental_models.py
+++ covid19_ode_inference/compartmental_models.py
@@ -10,7 +10,7 @@ from covid19_ode_inference.pytensor_op import create_and_register_jax
 
 def SIR_eqs(t, y, args):
     S, I, R = y
-    β, γ, N = args
+    β, (γ, N) = args
     dS_dt = -β.evaluate(t) * I * S / N
     dI_dt = β.evaluate(t) * I * S / N - γ * I
     dR_dt = -γ * I
@@ -22,12 +22,11 @@ def SIR_integrator(
     I_0,
     R_0,
     beta_t,
-    gamma,
-    N,
+    other_args,
     time,
     dt,
     t_0,
-    t_end,
+    t_1,
 ):
     cubic_interp = True
     if cubic_interp:
@@ -38,32 +37,155 @@ def SIR_integrator(
 
     term = diffrax.ODETerm(SIR_eqs)
     y0 = S_0, I_0, R_0
-    args = beta_interp, gamma, N
-    solver = diffrax.Dopri5()
+    args = beta_interp, other_args
+    solver = diffrax.Tsit5()
     saveat = diffrax.SaveAt(ts=time)
     sol = diffrax.diffeqsolve(
         term,
         solver,
         t_0,
-        t_end,
+        t_1,
         dt,
         y0,
         args=args,
         saveat=saveat,
         # adjoint=diffrax.BacksolveAdjoint(),
+        # adjoint=diffrax.RecursiveCheckpointAdjoint(checkpoints=int((t_1 - t_0) / dt)),
     )
     return sol.ys[0], sol.ys[1], sol.ys[2]
 
 
-def ODE_to_pytensor_op(integrator, num_outputs, time, dt, name, t_0=None, t_end=None):
+class CompModelsIntegrator:
+    """
+    Creates an integrator for compartmental models. If called with an ODE as arguments,
+    it returns
+    """
+
+    def __init__(
+        self,
+        ts_out,
+        t_0,
+        dt,
+        ts_arg=None,
+        interp="cubic",
+        solver=diffrax.Tsit5(),
+        t_1=None,
+    ):
+        self.ts_out = ts_out
+        self.t_0 = t_0
+        if t_1 is None:
+            t_1 = max(self.ts_out)
+        self.t_1 = float(t_1)
+        self.dt = dt
+        self.ts_arg = ts_arg
+        self.interp = interp
+        self.solver = solver
+
+    def get_func(self, ODE):
+        def integrator(y0, arg_t=None, constant_args=None):
+            if arg_t is not None:
+                arg_t_interp = interpolation(
+                    ts=self.ts_arg, x=arg_t, method=self.interp
+                )
+
+            term = diffrax.ODETerm(ODE)
+
+            if arg_t is None:
+                args = constant_args
+            elif constant_args is None:
+                args = arg_t_interp
+            else:
+                args = (
+                    arg_t_interp,
+                    constant_args,
+                )
+            saveat = diffrax.SaveAt(ts=self.ts_out)
+            sol = diffrax.diffeqsolve(
+                term,
+                self.solver,
+                self.t_0,
+                self.t_1,
+                self.dt,
+                tuple(y0),
+                args=args,
+                saveat=saveat,
+                # adjoint=diffrax.BacksolveAdjoint(),
+            )
+
+            return tuple([sol.ys[i] for i in range(len(y0))])
+
+        return integrator
+
+    def get_Op(self, ODE, name, return_shapes=((),)):
+        integrator = self.get_func(ODE)
+
+        pytensor_op = create_and_register_jax(
+            integrator,
+            output_types=[
+                TensorType(
+                    dtype="float64", shape=tuple([len(self.ts_out)] + list(shape))
+                )
+                for shape in return_shapes
+            ],
+            name=name,
+        )
+        return pytensor_op
+
+
+def interpolation(ts, x, method):
+    if ts is not None:
+        if method == "cubic":
+            coeffs = diffrax.backward_hermite_coefficients(ts, x)
+            interp = diffrax.CubicInterpolation(ts, coeffs)
+        elif method == "linear":
+            interp = diffrax.LinearInterpolation(ts, x)
+        else:
+            raise RuntimeError(
+                f'Interpoletion method {self.interp} not known, possibilities are "cubic" or "linear"'
+            )
+    return interp
+
+
+def comp_models_integrator(
+    ODE_eqs, y0, arg_t, ts_arg, other_args, ts_out, t_0, t_1, dt, interp="cubic"
+):
+    if interp == "cubic":
+        coeffs_beta = diffrax.backward_hermite_coefficients(ts_arg, arg_t)
+        arg_t_interp = diffrax.CubicInterpolation(ts_arg, coeffs_beta)
+    elif interp == "linear":
+        arg_t_interp = diffrax.LinearInterpolation(ts_arg, arg_t)
+
+    term = diffrax.ODETerm(SIR_eqs)
+    args = (
+        arg_t_interp,
+        other_args,
+    )
+    solver = diffrax.Tsit5()
+    saveat = diffrax.SaveAt(ts=time)
+    sol = diffrax.diffeqsolve(
+        term,
+        solver,
+        t_0,
+        t_1,
+        dt,
+        y0,
+        args=args,
+        saveat=saveat,
+        # adjoint=diffrax.BacksolveAdjoint(),
+        # adjoint=diffrax.RecursiveCheckpointAdjoint(checkpoints=int(t_1 - t_0) / dt),
+    )
+    return tuple([sol.ys[i] for i in range(len(y0))])
+
+
+def ODE_to_pytensor_op(integrator, num_outputs, time, dt, name, t_0=None, t_1=None):
     if t_0 is None:
         t_0 = min(time)
-    if t_end is None:
-        t_end = max(time)
-    t_end = float(t_end)
+    if t_1 is None:
+        t_1 = max(time)
+    t_1 = float(t_1)
 
     ODE_with_defined_time = functools.partial(
-        integrator, time=time, t_0=t_0, t_end=t_end, dt=dt
+        integrator, time=time, t_0=t_0, t_1=t_1, dt=dt
     )
 
     pytensor_op = create_and_register_jax(
--- covid19_ode_inference/pytensor_op.py
+++ covid19_ode_inference/pytensor_op.py
@@ -1,8 +1,8 @@
-import functools
 import inspect
 
-import jax
 import numpy as np
+import jax
+from jax.tree_util import tree_flatten, tree_unflatten
 
 import pytensor.tensor as pt
 from pytensor.graph import Apply, Op
@@ -26,7 +26,7 @@ def create_and_register_jax(
     Parameters
     ----------
     jax_func : jax jittable function
-        function for which the node is created, can return multiple tensors.
+        function for which the node is created, can return multiple tensors as a tuple.
     output_types : list of pt.TensorType
         The shape of the TensorType has to be defined.
     input_dtype : str
@@ -40,39 +40,67 @@ def create_and_register_jax(
         and compilable with both JAX and C backend.
 
     """
+
+    # def jax_func_flat(*inputs, **kwinputs):
+
     jitted_sol_op_jax = jax.jit(jax_func)
     len_gz = len(output_types)
 
     def vjp_sol_op_jax(*args):
         y0 = args[:-len_gz]
-        gz = tuple(args[-len_gz:])
+        gz = args[-len_gz:]
         _, vjp_fn = jax.vjp(jax_func, *y0)
         return vjp_fn(gz)
+
+    # def vjp_sol_op_jax(y0, gz):
+    #    _, vjp_fn = jax.vjp(jax_func, *y0)
+    #    return vjp_fn(gz)
 
     jitted_vjp_sol_op_jax = jax.jit(vjp_sol_op_jax)
 
     class SolOp(Op):
+        def __init__(self):
+            self.vjp_sol_op = None
+
         def make_node(self, *inputs, **kwinputs):
             # Convert keyword inputs to positional arguments
             func_signature = inspect.signature(jax_func)
+            for key in kwinputs.keys():
+                if not key in func_signature.parameters:
+                    raise RuntimeError(
+                        f"Keyword argument <{key}> not found in function signature. "
+                        f"**kwargs are not supported in the definition of the function"
+                    )
             arguments = func_signature.bind(*inputs, **kwinputs)
+            arguments.apply_defaults()
             all_inputs = arguments.args
 
             # Convert our inputs to symbolic variables
-            all_inputs = [
-                pt.as_tensor_variable(inp).astype(input_dtype) for inp in all_inputs
+            all_inputs_flat, self.input_tree = tree_flatten(all_inputs)
+
+            all_inputs_flat = [
+                pt.as_tensor_variable(inp).astype(input_dtype)
+                for inp in all_inputs_flat
             ]
 
             # Define our output variables
             outputs = [pt.as_tensor_variable(type()) for type in output_types]
 
-            return Apply(self, all_inputs, outputs)
+            self.vjp_sol_op = VJPSolOp(self.input_tree)
+
+            return Apply(self, all_inputs_flat, outputs)
 
         def perform(self, node, inputs, outputs):
             """This function is called by the C backend, thus the numpy conversion"""
+            inputs = tree_unflatten(self.input_tree, inputs)
             results = jitted_sol_op_jax(*inputs)
             for i, _ in enumerate(output_types):
                 outputs[i][0] = np.array(results[i], output_types[i].dtype)
+
+        def perform_jax(self, *inputs):
+            inputs = tree_unflatten(self.input_tree, inputs)
+            results = jitted_sol_op_jax(*inputs)
+            return results
 
         def grad(self, inputs, output_gradients):
             # If a output is not used, it is disconnected and doesn't have a gradient.
@@ -80,46 +108,69 @@ def create_and_register_jax(
             for i, otype in enumerate(output_types):
                 if isinstance(output_gradients[i].type, DisconnectedType):
                     output_gradients[i] = pt.zeros(otype.shape, otype.dtype)
-            result = vjp_sol_op(inputs, output_gradients)
+
+            inputs_unflat = tree_unflatten(self.input_tree, inputs)
+            result = self.vjp_sol_op(inputs_unflat, output_gradients)
+
+            # inputs_unflat = tree_unflatten(self.input_tree, inputs)
+            # result = jitted_vjp_sol_op_jax(inputs, output_gradients)
+            # results, _ = tree_flatten(result)
             results = [result[i] for i, _ in enumerate(inputs)]
-            return results
+            # results = tree_unflatten(self.input_tree, results)
+            return tuple(results)
 
     # vector-jacobian product Op
     class VJPSolOp(Op):
+        def __init__(self, input_tree_def):
+            self.input_tree_def = input_tree_def
+
         def make_node(self, y0, gz):
+            y0_flat, self.input_tree_def = tree_flatten(y0)
             inputs = [
                 pt.as_tensor_variable(
                     _y,
                 ).astype(input_dtype)
-                for _y in y0
+                for _y in y0_flat
             ] + list(gz)
 
-            outputs = [input.type() for input in y0]
+            inputs_unflat = tuple(list(y0) + list(gz))
+            _, self.full_input_tree_def = tree_flatten(inputs_unflat)
+
+            outputs = [input.type() for input in y0_flat]
             return Apply(self, inputs, outputs)
 
         def perform(self, node, inputs, outputs):
+            inputs = tree_unflatten(self.full_input_tree_def, inputs)
             results = jitted_vjp_sol_op_jax(*inputs)
+            results, _ = tree_flatten(results)
             for i, result in enumerate(results):
                 outputs[i][0] = np.array(result, input_dtype)
+
+        def perform_jax(self, *inputs):
+            inputs = tree_unflatten(self.full_input_tree_def, inputs)
+            results = jitted_vjp_sol_op_jax(*inputs)
+            results, _ = tree_flatten(results)
+            return results
 
     if name is None:
         name = jax_func.__name__
     SolOp.__name__ = name
     SolOp.__qualname__ = ".".join(SolOp.__qualname__.split(".")[:-1] + [name])
+
     VJPSolOp.__name__ = "VJP" + name
     VJPSolOp.__qualname__ = ".".join(
         VJPSolOp.__qualname__.split(".")[:-1] + ["VJP" + name]
     )
 
     sol_op = SolOp()
-    vjp_sol_op = VJPSolOp()
+    # vjp_sol_op = VJPSolOp()
 
     @jax_funcify.register(SolOp)
     def sol_op_jax_funcify(op, **kwargs):
-        return jax_func
+        return sol_op.perform_jax
 
     @jax_funcify.register(VJPSolOp)
     def vjp_sol_op_jax_funcify(op, **kwargs):
-        return vjp_sol_op_jax
+        return sol_op.vjp_sol_op.perform_jax
 
     return sol_op
